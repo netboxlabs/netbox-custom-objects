f5ea980c4c193baad02dc30005262277751c02bc
Brian Tiemann <btman@mac.com>
2025-02-18 15:54:37 -0500

    Render link to ServiceMapping from any relation object

--- netbox_service_mappings/template_content.py
+++ netbox_service_mappings/template_content.py
@@ -1,8 +1,11 @@
+from django.contrib.contenttypes.models import ContentType
 from netbox.plugins import PluginTemplateExtension
+from .models import MappingRelation
 
 __all__ = (
     'MappingSchema',
     'MappingElements',
+    'MappingLink',
     'template_extensions',
 )
 
@@ -27,7 +30,28 @@ class MappingElements(PluginTemplateExtension):
         return instance.formatted_data
 
 
+class MappingLink(PluginTemplateExtension):
+
+    def left_page(self):
+        if not (instance := self.context['object']):
+            return ''
+
+        content_type = ContentType.objects.get_for_model(instance)
+        relations = MappingRelation.objects.filter(field__content_type=content_type, object_id=instance.pk)
+        if not relations.exists():
+            return ''
+
+        result = '<h2 class="card-header">Service Mappings</h2>'
+        result += '<ul>'
+        for relation in relations:
+            url = relation.mapping.get_absolute_url()
+            result += f'<li><a href="{url}">{relation.mapping}</a></li>'
+        result += '</ul>'
+        return result
+
+
 template_extensions = (
     MappingSchema,
     MappingElements,
+    MappingLink,
 )
