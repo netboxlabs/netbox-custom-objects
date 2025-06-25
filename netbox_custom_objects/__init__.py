6a9c5ef2156124670a40a74942f7a4f6ad88fb2e
Arthur Hanson <worldnomad@gmail.com>
2025-06-25 07:53:11 -0700

    NPL-279 fix delete with tags (#14)

--- netbox_custom_objects/__init__.py
+++ netbox_custom_objects/__init__.py
@@ -1,5 +1,5 @@
 from netbox.plugins import PluginConfig
-
+from django.apps import apps
 
 # Plugin Configuration
 class CustomObjectsPluginConfig(PluginConfig):
@@ -14,6 +14,31 @@ class CustomObjectsPluginConfig(PluginConfig):
     required_settings = []
     template_extensions = "template_content.template_extensions"
 
+    def get_model(self, model_name, require_ready=True):
+        if require_ready:
+            self.apps.check_models_ready()
+        else:
+            self.apps.check_apps_ready()
+
+        if model_name.lower() in self.models:
+            return self.models[model_name.lower()]
+        
+        from .models import CustomObjectType
+        if "table" not in model_name.lower() or "model" not in model_name.lower():
+            raise LookupError(
+                "App '%s' doesn't have a '%s' model." % (self.label, model_name)
+            )
+
+        custom_object_type_id = int(model_name.replace("table", "").replace("model", ""))
+
+        try:
+            obj = CustomObjectType.objects.get(pk=custom_object_type_id)
+        except CustomObjectType.DoesNotExist:
+            raise LookupError(
+                "App '%s' doesn't have a '%s' model." % (self.label, model_name)
+            )
+        return obj.get_model()
+    
     def ready(self):
         super().ready()
 
