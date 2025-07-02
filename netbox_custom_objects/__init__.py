86f3ed60276e4e536086e73a83a6a783818035c6
Arthur Hanson <worldnomad@gmail.com>
2025-07-02 10:55:08 -0700

    update version number (#63)

--- netbox_custom_objects/__init__.py
+++ netbox_custom_objects/__init__.py
@@ -6,7 +6,7 @@ class CustomObjectsPluginConfig(PluginConfig):
     name = "netbox_custom_objects"
     verbose_name = "Custom Objects"
     description = "A plugin to manage custom objects in NetBox"
-    version = "0.1"
+    version = "0.1.0"
     base_url = "custom-objects"
     min_version = "4.2.0"
     default_settings = {}
