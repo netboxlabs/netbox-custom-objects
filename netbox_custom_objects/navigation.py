e42bc6a9afcfcdba567250bd4defab4e663d5e11
Brian Tiemann <btman@mac.com>
2025-07-13 20:19:45 -0400

    Make url a property on MenuItem/PluginMenuItem etc, overridable via a setter

--- netbox_custom_objects/navigation.py
+++ netbox_custom_objects/navigation.py
@@ -27,27 +27,27 @@ class CustomObjectTypeMenuItems:
         CustomObjectType = apps.get_model(APP_LABEL, "CustomObjectType")
         for custom_object_type in CustomObjectType.objects.all():
             model = custom_object_type.get_model()
-            yield PluginMenuItem(
-                link=None,
-                url=reverse(
-                    f"plugins:{APP_LABEL}:customobject_list",
-                    kwargs={"custom_object_type": custom_object_type.name.lower()},
-                ),
-                link_text=_(title(model._meta.verbose_name_plural)),
-                buttons=(
-                    PluginMenuButton(
-                        None,
-                        _("Add"),
-                        "mdi mdi-plus-thick",
-                        url=reverse(
-                            f"plugins:{APP_LABEL}:customobject_add",
-                            kwargs={
-                                "custom_object_type": custom_object_type.name.lower()
-                            },
-                        ),
-                    ),
-                ),
+            add_button = PluginMenuButton(
+                None,
+                _("Add"),
+                "mdi mdi-plus-thick",
             )
+            add_button.url = reverse(
+                f"plugins:{APP_LABEL}:customobject_add",
+                kwargs={
+                    "custom_object_type": custom_object_type.name.lower()
+                },
+            )
+            menu_item = PluginMenuItem(
+                link=None,
+                link_text=_(title(model._meta.verbose_name_plural)),
+                buttons=(add_button,),
+            )
+            menu_item.url = reverse(
+                f"plugins:{APP_LABEL}:customobject_list",
+                kwargs={"custom_object_type": custom_object_type.name.lower()},
+            )
+            yield menu_item
 
 
 current_version = version.parse(settings.RELEASE.version)
