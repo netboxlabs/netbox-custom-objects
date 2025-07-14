7128a0f2f223d4f29f7e9b3006db04b14ebbcd8a
Arthur <worldnomad@gmail.com>
2025-07-14 10:19:22 -0700

    NPL-374 Add tags

--- netbox_custom_objects/__init__.py
+++ netbox_custom_objects/__init__.py
@@ -41,7 +41,6 @@ class CustomObjectsPluginConfig(PluginConfig):
         return obj.get_model()
     '''
 
-    '''   
     def ready(self):
         import netbox_custom_objects.signals
         
@@ -77,7 +76,6 @@ class CustomObjectsPluginConfig(PluginConfig):
             print(f"Warning: Could not initialize custom object models: {e}")
             
         super().ready()
-    '''   
 
 
 config = CustomObjectsPluginConfig
