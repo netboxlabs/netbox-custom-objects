03d5da58b1652753065e1d509f631a55d7ddc1ca
Arthur <worldnomad@gmail.com>
2025-07-15 08:13:27 -0700

    NPL-374 cleanup

--- netbox_custom_objects/__init__.py
+++ netbox_custom_objects/__init__.py
@@ -1,67 +1,16 @@
 from netbox.plugins import PluginConfig
-from django.apps import apps
+
 
 # Plugin Configuration
 class CustomObjectsPluginConfig(PluginConfig):
     name = "netbox_custom_objects"
     verbose_name = "Custom Objects"
     description = "A plugin to manage custom objects in NetBox"
-    version = "0.1"
+    version = "0.1.0"
     base_url = "custom-objects"
     min_version = "4.2.0"
-    # max_version = "3.5.0"
     default_settings = {}
     required_settings = []
     template_extensions = "template_content.template_extensions"
-
-    '''
-    def get_model(self, model_name, require_ready=True):
-        return super().get_model(model_name, require_ready)
-        if require_ready:
-            self.apps.check_models_ready()
-        else:
-            self.apps.check_apps_ready()
-
-        if model_name.lower() in self.models:
-            return self.models[model_name.lower()]
-        
-        from .models import CustomObjectType
-        if "table" not in model_name.lower() or "model" not in model_name.lower():
-            raise LookupError(
-                "App '%s' doesn't have a '%s' model." % (self.label, model_name)
-            )
-
-        custom_object_type_id = int(model_name.replace("table", "").replace("model", ""))
-
-        try:
-            obj = CustomObjectType.objects.get(pk=custom_object_type_id)
-        except CustomObjectType.DoesNotExist:
-            raise LookupError(
-                "App '%s' doesn't have a '%s' model." % (self.label, model_name)
-            )
-        return obj.get_model()
-    '''
-
-    def ready(self):
-        # Ensure all dynamic models are created and registered during startup
-        # This prevents ContentType race conditions with Bookmark operations
-        try:
-            from .models import CustomObjectType
-            
-            # Only run this after the database is ready
-            if apps.is_installed('django.contrib.contenttypes'):
-                for custom_object_type in CustomObjectType.objects.all():
-                    try:
-                        custom_object_type.get_model()
-
-                    except Exception as e:
-                        # Log but don't fail startup
-                        print(f"Warning: Could not initialize model for CustomObjectType {custom_object_type.id}: {e}")
-        except Exception as e:
-            # Don't fail plugin startup if there are issues
-            print(f"Warning: Could not initialize custom object models: {e}")
-            
-        super().ready()
-
 
 config = CustomObjectsPluginConfig
