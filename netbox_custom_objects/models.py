9203ea4665da0e0aa9294111118aa59e5c8314fe
Arthur <worldnomad@gmail.com>
2025-08-22 13:49:07 -0700

    183 fix deletion of COT when Object Field pointing to it

--- netbox_custom_objects/models.py
+++ netbox_custom_objects/models.py
@@ -553,14 +553,14 @@ class CustomObjectType(PrimaryModel):
         self.clear_model_cache(self.id)
 
         model = self.get_model()
+
+        # Delete all CustomObjectTypeFields that reference this CustomObjectType
+        for field in CustomObjectTypeField.objects.filter(related_object_type=self.content_type):
+            field.delete()
+
         object_type = ObjectType.objects.get_for_model(model)
         ObjectChange.objects.filter(changed_object_type=object_type).delete()
         super().delete(*args, **kwargs)
-
-        # Delete all CustomObjectTypeFields that reference this CustomObjectType
-        CustomObjectTypeField.objects.filter(
-            related_object_type=self.content_type
-        ).delete()
 
         # Temporarily disconnect the pre_delete handler to skip the ObjectType deletion
         # TODO: Remove this disconnect/reconnect after ObjectType has been exempted from handle_deleted_object
