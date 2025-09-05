d0723a0d433768a7f33778ada567de9d8bb35dfc
Arthur Hanson <worldnomad@gmail.com>
2025-09-05 14:11:32 -0700

    bump to 0.3.1 (#208)

--- netbox_custom_objects/__init__.py
+++ netbox_custom_objects/__init__.py
@@ -45,7 +45,7 @@ class CustomObjectsPluginConfig(PluginConfig):
     name = "netbox_custom_objects"
     verbose_name = "Custom Objects"
     description = "A plugin to manage custom objects in NetBox"
-    version = "0.3.0"
+    version = "0.3.1"
     base_url = "custom-objects"
     min_version = "4.4.0"
     default_settings = {}
