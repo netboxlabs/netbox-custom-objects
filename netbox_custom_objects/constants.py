3f3ffe0fb2c203761aa531285d689df3ec8ecfad
Arthur <worldnomad@gmail.com>
2025-12-09 14:23:41 -0800

    #299 add extra reserved words to reserved CO field names

--- netbox_custom_objects/constants.py
+++ netbox_custom_objects/constants.py
@@ -9,17 +9,41 @@ APP_LABEL = "netbox_custom_objects"
 
 # Field names that are reserved and cannot be used for custom object fields
 RESERVED_FIELD_NAMES = [
+    # Django model internals
+    "_meta",
+    "_state",
+    "DoesNotExist",
+    "MultipleObjectsReturned",
+    "objects",
+    # Primary key fields
+    "id",
+    "pk",
+    # Django model methods
+    "clean",
+    "delete",
+    "full_clean",
+    "refresh_from_db",
+    "save",
+    # Custom object specific
+    "clone",
+    "custom_object_type",
+    "custom_object_type_id",
+    "custom_field_data",
+    "model",
+    # Change logging
+    "created",
+    "last_updated",
+    "serialize_object",
+    "snapshot",
+    "to_objectchange",
+    # Generic relations from mixins
     "bookmarks",
     "contacts",
-    "created",
-    "custom_field_data",
-    "id",
     "images",
     "jobs",
     "journal_entries",
-    "last_updated",
-    "pk",
     "subscriptions",
     "tags",
-    "model",
+    # URL methods
+    "get_absolute_url",
 ]
