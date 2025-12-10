ebbc72798ca70bdd0018249e783e3458fa4b93bc
Arthur Hanson <worldnomad@gmail.com>
2025-12-09 16:55:27 -0800

    Fixes #299: add model to reserved CO field names (#321)
    * #299 add model to reserved CO field names
    
    * #299 add extra reserved words to reserved CO field names
    
    * #299 add extra reserved words to reserved CO field names

--- netbox_custom_objects/constants.py
+++ netbox_custom_objects/constants.py
@@ -7,18 +7,37 @@ INCLUDE_MODELS = (
 
 APP_LABEL = "netbox_custom_objects"
 
-# Field names that are reserved and cannot be used for custom object fields
+# Field names that are reserved and cannot be used for custom object fields.
+# Keep in alphabetical order for ease of reading error message.
 RESERVED_FIELD_NAMES = [
+    "_meta",
+    "_state",
+    "DoesNotExist",
+    "MultipleObjectsReturned",
     "bookmarks",
+    "clean",
+    "clone",
     "contacts",
     "created",
     "custom_field_data",
+    "custom_object_type",
+    "custom_object_type_id",
+    "delete",
+    "full_clean",
+    "get_absolute_url",
     "id",
     "images",
     "jobs",
     "journal_entries",
     "last_updated",
+    "model",
+    "objects",
     "pk",
+    "refresh_from_db",
+    "save",
+    "serialize_object",
+    "snapshot",
     "subscriptions",
     "tags",
+    "to_objectchange",
 ]
