931d4f07299f507983c7eda7be5b8389a194b4a5
Brian Tiemann <btman@mac.com>
2026-04-20 21:22:11 -0400

    Ruff fix

--- netbox_custom_objects/migrations/0005_customobjecttypefield_context_and_more.py
+++ netbox_custom_objects/migrations/0005_customobjecttypefield_context_and_more.py
@@ -20,15 +20,48 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customobjecttypefield',
             name='related_name',
-            field=models.CharField(blank=True, max_length=100, validators=[django.core.validators.RegexValidator(message='Only lowercase alphanumeric characters and underscores are allowed.', regex='^[a-z0-9_]+$'), django.core.validators.RegexValidator(flags=re.RegexFlag['IGNORECASE'], inverse_match=True, message='Double underscores are not permitted in the reverse relation name.', regex='__')]),
+            field=models.CharField(
+                blank=True,
+                max_length=100,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        message='Only lowercase alphanumeric characters and underscores are allowed.',
+                        regex='^[a-z0-9_]+$'
+                    ),
+                    django.core.validators.RegexValidator(
+                        flags=re.RegexFlag['IGNORECASE'],
+                        inverse_match=True,
+                        message='Double underscores are not permitted in the reverse relation name.',
+                        regex='__',
+                    ),
+                ]
+            ),
         ),
         migrations.AlterField(
             model_name='customobjecttypefield',
             name='name',
-            field=models.CharField(max_length=50, validators=[django.core.validators.RegexValidator(message='Only lowercase alphanumeric characters and underscores are allowed.', regex='^[a-z0-9_]+$'), django.core.validators.RegexValidator(flags=re.RegexFlag['IGNORECASE'], inverse_match=True, message='Double underscores are not permitted in custom object field names.', regex='__')]),
+            field=models.CharField(
+                max_length=50,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        message='Only lowercase alphanumeric characters and underscores are allowed.',
+                        regex='^[a-z0-9_]+$'
+                    ),
+                    django.core.validators.RegexValidator(
+                        flags=re.RegexFlag['IGNORECASE'],
+                        inverse_match=True,
+                        message='Double underscores are not permitted in custom object field names.',
+                        regex='__',
+                    ),
+                ]
+            ),
         ),
         migrations.AddConstraint(
             model_name='customobjecttypefield',
-            constraint=models.UniqueConstraint(condition=models.Q(('related_name__gt', '')), fields=('related_object_type', 'related_name'), name='netbox_custom_objects_customobjecttypefield_unique_related_name'),
+            constraint=models.UniqueConstraint(
+                condition=models.Q(('related_name__gt', '')),
+                fields=('related_object_type', 'related_name'),
+                name='netbox_custom_objects_customobjecttypefield_unique_related_name',
+            ),
         ),
     ]
